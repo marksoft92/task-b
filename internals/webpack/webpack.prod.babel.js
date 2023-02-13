373d4471dd3d3995dbffa9ff27264797ec140754
Dawid Bieńkowski <49159845+marksoft92@users.noreply.github.com>
2023-02-13 12:04:37 +0100

    Update webpack.prod.babel.js
    fix img fav

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -124,15 +124,7 @@ module.exports = require('./webpack.base.babel')({
       inject: true,
       ios: true,
       icons: [
-        {
-          src: path.resolve('app/images/icon-512x512.png'),
-          sizes: [72, 96, 128, 144, 192, 384, 512],
-        },
-        {
-          src: path.resolve('app/images/icon-512x512.png'),
-          sizes: [120, 152, 167, 180],
-          ios: true,
-        },
+        
       ],
     }),
 
